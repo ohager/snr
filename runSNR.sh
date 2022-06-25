f06c3e8d2a97102a88bcb0c93c1016ce166071a4
pir8radio <pir8radio@gmail.com>
2022-06-25 11:07:29 -0500

    Update runSNR.sh
    updated text info

--- runSNR.sh
+++ runSNR.sh
@@ -1,18 +1,19 @@
 # This script runs in an infinite loop, waking up every morning at 7:00AM
-# Pir8Radio 2022
+# Pir8Radio
 
 WAKEUP="07:00" # Wake up at this time tomorrow and run a command
 while :
 do
     SECS=$(expr `date -d "tomorrow $WAKEUP" +%s` - `date -d "now" +%s`)
     echo "`date +"%Y-%m-%d %T"`| Will sleep for $SECS seconds."
-    sleep $SECS &  # We sleep in the background to make the script interruptible
+    sleep $SECS &  # We sleep in the background to make the screipt interruptible
     wait $!
     echo "`date +"%Y-%m-%d %T"`| Waking up"
     
-    # Run your command here, replace paths with your real snr paths
-    /home/signum/Signum_Apps/snr/snr-cli.js queue 3.3.3 80 -x
+    # Check that nodes meet requirements X.X.X (version) XX (uptime %)
+    /home/signum/Signum_Apps/snr/snr-cli.js queue 3.4.1 80 -x
     wait
+    # Pay everyone that passed check above
     /home/signum/Signum_Apps/snr/snr-cli.js pay -x
 
 done
